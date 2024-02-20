c0b8c336a1774b87e09765dd7e807e954c62e2d7
son <son@mail.ru>
2024-02-20 17:10:10 +0500

    init

--- main.py
+++ main.py
@@ -3,6 +3,16 @@ import pprint
 with open('bott.txt') as f:
     token = f.read()
 
-endPoint = f'https://api.telegram.org/bot{token}/getMe'
+# endPoint = f'https://api.telegram.org/bot{token}/getMe'
+# res = requests.get(endPoint).json()
+# pprint.pprint(res)
+''''''
+endPoint = f'https://api.telegram.org/bot{token}/getUpdates'
 res = requests.get(endPoint).json()
 pprint.pprint(res)
+chatID = res['result'][0]['message']['chat']['id']
+print(chatID)
+''''''
+endPoint = f'https://api.telegram.org/bot{token}/sendMessage'
+params = {'chat_id': chatID, 'text': "гавр моя жина" }
+res = requests.get(endPoint, params = params).json()
